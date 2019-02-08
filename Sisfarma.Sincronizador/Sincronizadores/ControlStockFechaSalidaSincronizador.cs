58d21cfb025b4b4ab928c47081e3d1bc2e063fad
Federico Fernández <fede.fernandez.it@gmail.com>
2019-02-08 04:43:22 -0300

    upgrade control fecha salida

--- Sisfarma.Sincronizador/Sincronizadores/ControlStockFechaSalidaSincronizador.cs
+++ Sisfarma.Sincronizador/Sincronizadores/ControlStockFechaSalidaSincronizador.cs
@@ -3,6 +3,7 @@ using Sisfarma.Sincronizador.Farmatic;
 using Sisfarma.Sincronizador.Fisiotes;
 using Sisfarma.Sincronizador.Helpers;
 using Sisfarma.Sincronizador.Sincronizadores.SuperTypes;
+using System.Linq;
 using static Sisfarma.Sincronizador.Fisiotes.Repositories.ConfiguracionesRepository;
 
 namespace Sisfarma.Sincronizador.Sincronizadores
@@ -25,6 +26,13 @@ namespace Sisfarma.Sincronizador.Sincronizadores
             var fechaActualizacionStock = Calculator.CalculateFechaActualizacion(configuracion);
             
             var articulosWithIva = farmatic.Articulos.GetByFechaUltimaSalidaGreaterOrEqual(fechaActualizacionStock);
+            if (!articulosWithIva.Any())
+            {
+                _fisiotes.Configuraciones.Update(FIELD_STOCK_SALIDA, "0");
+                _fisiotes.Medicamentos.ResetPorDondeVoy();
+                return;
+            }
+
             foreach (var articulo in articulosWithIva)
             {
                 _cancellationToken.ThrowIfCancellationRequested();
@@ -36,6 +44,12 @@ namespace Sisfarma.Sincronizador.Sincronizadores
                 var medicamento = fisiotes.Medicamentos.GetOneOrDefaultByCodNacional(articulo.IdArticu);
 
                 SincronizarMedicamento(fisiotes, medicamento, medicamentoGenerado);
+            }
+
+            if (_farmatic.Articulos.GetControlArticuloFisrtOrDefault(articulosWithIva.Last().IdArticu) == null)
+            {
+                _fisiotes.Configuraciones.Update(FIELD_STOCK_SALIDA, "0");
+                _fisiotes.Medicamentos.ResetPorDondeVoySinStock();
             }
         }
     }
